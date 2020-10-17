87cd6402fed7797ea6bfed9fd038560e06fbf59f
Eva Klímová <enovotn@gmail.com>
2020-10-17 20:09:13 +0200

    modify the code so that it can be used for cheerio scraper

--- aktualne.js
+++ aktualne.js
@@ -1,30 +1,32 @@
-// Settings for Web Scraper - Aktualne:
+// Settings for Cheerio - Aktualne:
 
-// The function accepts a single argument: the "context" object.
-// For a complete list of its properties and functions,
-// see https://apify.com/apify/web-scraper#page-function 
 async function pageFunction(context) {
-    // jQuery is handy for finding DOM elements and extracting data from them.
-    // To use it, make sure to enable the "Inject jQuery" option.
-    const $ = context.jQuery; 
-    
+    const { $, request, log } = context;
+
+    // The "$" property contains the Cheerio object which is useful
+    // for querying DOM elements and extracting data from them.
     const pageTitle = $('h1.article-title').first().text().trim();
     const pageAuthor = $('a.author__name').text().trim();
     const pageOpener = $('div.article__perex').first().text().trim();
     const pageText = $('div.article__content').text().trim();
     const pageDate = $('div.author__date').text().trim();
-    const pageSection = $('a.header__menu__name').text().trim(); 
-   
+    const pageSection = $('a.header__menu__name').text().trim();
+
+    // The "request" property contains various information about the web page loaded. 
+    const url = request.url;
+    
+    // Use "log" object to print information to actor log.
+    log.info('Aktualne', { url, pageTitle });
+
     // Return an object with the data extracted from the page.
     // It will be stored to the resulting dataset.
-     return {
-        url: context.request.url,
+    return {
+        url,
         pageTitle,
         pageAuthor,
         pageOpener,
         pageText,
         pageDate,
         pageSection
-        
     };
 }
