6169f69ed0099025166312fe3854eec1dada8dfd
Karolína Horká <karo.horka@gmail.com>
2020-10-17 15:54:00 +0200

    modify the code so it can be used in the Cheerio scraper

--- idnes.js
+++ idnes.js
@@ -1,16 +1,16 @@
-// The function accepts a single argument: the "context" object.
+// New settings for Cheerio scraper
 
 async function pageFunction(context) {
-    // jQuery is handy for finding DOM elements and extracting data from them.
-    // To use it, make sure to enable the "Inject jQuery" option.
-    const $ = context.jQuery;
+    const { $, request, log } = context;
+
+    // The "$" property contains the Cheerio object which is useful
+    // for querying DOM elements and extracting data from them.
     const pageTitle = $('title').first().text().trim();
     const pageText = $('div#art-text.text').text().trim();
     const pageDate = $('span.time-date').text().trim();
     const pageOpener = $('div.opener').text().trim();
     const pageAuthor = $('div.authors').text().trim();
     const pageSection = $('div.portal-g2a h3').text().trim();
-
 
 
     // Return an object with the data extracted from the page.
@@ -23,6 +23,5 @@ async function pageFunction(context) {
         pageAuthor,
         pageTitle,
         url: context.request.url
-        
     };
 }
